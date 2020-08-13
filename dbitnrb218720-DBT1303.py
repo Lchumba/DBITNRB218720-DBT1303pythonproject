40712ba29766832ff517770dd78d62cf289e20f4
Lchumba <69582659+Lchumba@users.noreply.github.com>
2020-08-13 23:24:14 +0300

    Add files via upload

---
+++ dbitnrb218720-DBT1303.py
@@ -0,0 +1,36 @@
+'''Hi this is my  security pass project by lilian chumba Dbitnrb218720 DBT1303'''
+
+#THIS SYSTEM IS TO WORK WITH AIM OF MAKING SURE THAT ALL THAT VISIT THE NEIGHBOURHOOD ARE INVITED
+
+#BEFORE THE VISIT ''' Host is asked to provide a security code by askari
+
+code= float
+
+code = input("Enter your future visitation  approval code:")
+print(" your "  + code + " is Noted, Thank You")
+
+Visit_House_number = input("enter the house mapped to the code above")
+print(" your " + Visit_House_number + "has been mapped to "+ code )
+
+#On the day of the visit the Host shares the code with the expected guest privately
+
+'''Security asks the visitor for details'''
+
+geust= input( " Enter your Name :")
+print(" Welcome " + geust )
+
+Place_to_visit= input( "house number:" )
+print("you will be allowed to visit after input the code")
+
+
+#Code is requested
+
+
+code =input( "enter your code" )
+
+code = True
+print (" Welcome ")
+code = False
+print (" You can not pass ")
+
+
